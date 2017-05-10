34ced1862a57c5abb368ab94f6c9e8a189c7de04
Phuong <boingonline@gmail.com>
2017-05-10 11:41:54 +0700

    Update scripts

--- cli/script.py
+++ cli/script.py
@@ -22,6 +22,7 @@ def clean_vi_dictionary():
 
 
 ###########################################################################
+# This section is used to build lemma, type of word
 
 SOURCE_URL = "http://tratu.coviet.vn/hoc-tieng-anh/tu-dien/lac-viet/V-V/%s.html"
 import urllib
@@ -35,8 +36,10 @@ VERB = u'động từ'
 PRONN = u'đại từ'
 CONJ = u'kết từ'
 PREP = u'giới từ'
+ART = u'mạo từ'
+PROPN = u'danh từ riêng'
 
-LEMMA = [NOUN, ADJ, ADV, VERB, PRONN, CONJ, PREP]
+LEMMA = [NOUN, ADJ, ADV, VERB, PRONN, CONJ, PREP, ART, PRONN]
 LEMMAP = {
     NOUN: 'NOUN',
     ADJ: 'ADJ',
@@ -44,33 +47,79 @@ LEMMAP = {
     VERB: 'VERB',
     PRONN: 'PRONN',
     CONJ: 'CONJ',
-    PREP: 'PREP'
+    PREP: 'PREP',
+    ART: 'ART',
+    PROPN: 'PROPN'
 }
 
 
 def find_type_of_word(word):
+    if isinstance(word, str):
+        word = unicode(word, 'utf-8')
     url = SOURCE_URL % urllib.quote(word.encode('utf8'))
     req = requests.get(url)
     tree = html.fromstring(req.content)
     ub = tree.xpath('//div[@class="ub"]')
+    lemmas = []
     for lemma in ub:
         lemma = lemma.text_content().strip()
         if lemma in LEMMA:
-            yield LEMMAP[lemma]
+            lemmas.append(LEMMAP[lemma])
+    return lemmas
+
+
+def is_prop_noun(word):
+    words = word.split()
+    word_length = len(words)
+    found = 0
+    for w in words:
+        if w[0].istitle():
+            found += 1
+    if found == word_length:
+        return True
+    return word_length > 3 and found >= 2
 
 
 def build_word_type_data():
-    # file = open('data/words.txt', 'r')
-    # raw_data = file.read()
-    # file.close()
-    raw_data = u"""hoặc|một|nếu|bởi vì"""
+    file = open('data/words.txt', 'r')
+    raw_data = file.read()
+    file.close()
     counter = 0
+    max = 450
 
-    for word in raw_data.split('|'):
-        w = find_type_of_word(word)
-        print(word, "=")
-        for _w in w:
-            print(_w)
+    file_lemmas = dict()
+    file_prop_noun = open('data/processed/propnoun.txt', 'w')
+    file_undefined = open('data/processed/undefined.txt', 'w')
+    file_lemma = open('data/processed/lemma.txt', 'w')
+
+    def get_file(lemma):
+        if lemma not in file_lemmas:
+            file_lemmas[lemma] = open('data/processed/%s.txt' % lemma, 'w')
+        return file_lemmas[lemma]
+
+    def close_files():
+        file_prop_noun.close()
+        file_undefined.close()
+        file_lemma.close()
+        for key, file in file_lemmas.items():
+            file.close()
+
+    for word in raw_data.split('\n'):
+        counter += 1
+        if counter == max:
+            break
+        if is_prop_noun(word):
+            file_prop_noun.write('%s\n' % word)
+        else:
+            lemmas = find_type_of_word(word)
+            if lemmas:
+                for lemma in lemmas:
+                    get_file(lemma).write('%s\n' % word)
+                file_lemma.write('%s;%s\n' % (word, ','.join(lemmas)))
+            else:
+                file_undefined.write('%s\n' % word)
+
+    close_files()
 
 
 build_word_type_data()
